c48cd3d5692e55106944e648bb49b7e1ca40850f
Siavash Baratchi <siavashbaratchi@Siavashs-MacBook-Pro.local>
2016-11-12 14:33:05 -0800

    Committing the initial working version of the map view

---
+++ FillUp/LocationSearchTable.swift
@@ -0,0 +1,88 @@
+//
+//  LocationSearchTable.swift
+//  FillUP
+//
+//  Created by Siavash Baratchi on 11/9/16.
+//  Copyright © 2016 Siavash Baratchi. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class LocationSearchTable : UITableViewController {
+    
+    var matchingItems:[MKMapItem] = []
+    var mapView: MKMapView? = nil
+    var handleMapSearchDelegate:HandleMapSearch? = nil
+}
+
+extension LocationSearchTable : UISearchResultsUpdating {
+    
+    /*
+     This function sends the search text to MKlocalsearch library and get the results
+     and stores them into LocationSearchTable properties
+     */
+    func updateSearchResults(for searchControler: UISearchController) {
+        
+        guard let mapView = self.mapView,
+            let searchBarText = searchControler.searchBar.text else { return }
+        let request = MKLocalSearchRequest()
+        request.naturalLanguageQuery = searchBarText
+        request.region = mapView.region
+        let search = MKLocalSearch(request: request)
+        search.start { response, _ in
+            guard let response = response else {
+                return
+            }
+            self.matchingItems = response.mapItems
+            self.tableView.reloadData()
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return matchingItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        let selectedItem = matchingItems[indexPath.row].placemark
+        cell.textLabel?.text = selectedItem.name
+        cell.detailTextLabel?.text = parseAddress(selectedItem: selectedItem)
+        return cell
+    }
+    
+    
+    func parseAddress(selectedItem:MKPlacemark) -> String {
+        // put a space between "4" and "Melrose Place"
+        let firstSpace = (selectedItem.subThoroughfare != nil && selectedItem.thoroughfare != nil) ? " " : ""
+        // put a comma between street and city/state
+        let comma = (selectedItem.subThoroughfare != nil || selectedItem.thoroughfare != nil) && (selectedItem.subAdministrativeArea != nil || selectedItem.administrativeArea != nil) ? ", " : ""
+        // put a space between "Washington" and "DC"
+        let secondSpace = (selectedItem.subAdministrativeArea != nil && selectedItem.administrativeArea != nil) ? " " : ""
+        let addressLine = String(
+            format:"%@%@%@%@%@%@%@",
+            // street number
+            selectedItem.subThoroughfare ?? "",
+            firstSpace,
+            // street name
+            selectedItem.thoroughfare ?? "",
+            comma,
+            // city
+            selectedItem.locality ?? "",
+            secondSpace,
+            // state
+            selectedItem.administrativeArea ?? ""
+        )
+        return addressLine
+    }
+    
+}
+
+extension LocationSearchTable {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedItem = matchingItems[indexPath.row].placemark
+        handleMapSearchDelegate?.dropPinZoomIn(placemark: selectedItem)
+        dismiss(animated: true, completion: nil)
+    }
+}
--- FillUp/ViewController.swift
+++ FillUp/ViewController.swift
@@ -7,19 +7,115 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+
+protocol HandleMapSearch {
+    func dropPinZoomIn(placemark:MKPlacemark)
+}
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    var locationManager:CLLocationManager!
+    
+    //var resultSearchController:UISearchController! = nil
+    var resultSearchController = UISearchController(searchResultsController: nil)
+
+    //Map pin
+    var selectedPin:MKPlacemark? = nil
+    
+    @IBOutlet weak var FillUpMapView: MKMapView!
+    
+    //Search controler
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        // Setting up the location manager for centering map at the current location
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        // Centering map to the current location
+        self.centerMapOnLocation(initialLocation: locationManager.location!)
+        
+        
+        FillUpMapView.showsUserLocation = true
+        self.FillUpMapView.layer.borderWidth = 5
+        self.FillUpMapView.layer.borderColor = UIColor(red:222/255.0, green:225/255.0, blue:227/255.0, alpha: 1.0).cgColor
+        
+        
+        // instantiating locationSearchTable
+        let locationSearchTable = storyboard!.instantiateViewController(withIdentifier: "LocationSearchTable") as! LocationSearchTable
+        resultSearchController = UISearchController(searchResultsController: locationSearchTable)
+        resultSearchController.searchResultsUpdater = locationSearchTable
+        
+        
+        //setting locationtable map view
+        locationSearchTable.mapView = FillUpMapView
+        
+        
+        //Setting up the search bar
+        let searchBar = resultSearchController.searchBar
+        searchBar.sizeToFit()
+        searchBar.placeholder = "Search for places"
+        navigationItem.titleView = resultSearchController.searchBar
+        
+        
+        // Navigation bar behavior
+        resultSearchController.hidesNavigationBarDuringPresentation = false
+        resultSearchController.dimsBackgroundDuringPresentation = true
+        definesPresentationContext = true
+        
+        locationSearchTable.handleMapSearchDelegate = self
+
+        }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    func centerMapOnLocation(initialLocation: CLLocation) {
+        
+        let regionRadius: CLLocationDistance = 1000
 
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(initialLocation.coordinate,
+                                                                  regionRadius * 2.0, regionRadius * 2.0)
+        FillUpMapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    @IBAction func GoToCurrentLocarion(_ sender: UIButton) {
+        
+        self.locationManager.startUpdatingLocation()
+        
+        self.centerMapOnLocation(initialLocation: self.locationManager.location!)
+
+    }
+    
 }
+
+extension ViewController: HandleMapSearch {
+    func dropPinZoomIn(placemark:MKPlacemark){
+        // cache the pin
+        selectedPin = placemark
+        // clear existing pins
+        FillUpMapView.removeAnnotations(FillUpMapView.annotations)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = placemark.coordinate
+        annotation.title = placemark.name
+        if let city = placemark.locality,
+            let state = placemark.administrativeArea {
+            annotation.subtitle = "(city) (state)"
+        }
+        FillUpMapView.addAnnotation(annotation)
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegionMake(placemark.coordinate, span)
+        FillUpMapView.setRegion(region, animated: true)
+    }
+}
+
 
